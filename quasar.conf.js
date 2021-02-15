c028a3a5904cba9931e062bd9a86793d944c9ecf
GLinBoy <hojjatabedie@gmail.com>
2021-02-15 22:37:58 +0330

    🐛 FIX: change port number to 9090

--- quasar.conf.js
+++ quasar.conf.js
@@ -86,7 +86,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
-      port: 8080,
+      port: 9090,
       open: true // opens browser window automatically
     },
 
