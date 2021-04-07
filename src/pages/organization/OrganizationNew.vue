a37e14a892e8eaa2523716b4f098a58f02b54ea3
GLinBoy <hojjatabedie@gmail.com>
2021-04-07 13:53:29 +0430

    👌 IMPROVE: redesign organization page
    resolved #40

--- src/pages/organization/OrganizationNew.vue
+++ src/pages/organization/OrganizationNew.vue
@@ -1,83 +1,69 @@
 <template>
   <q-layout>
     <q-page-container>
-      <q-page class="row padding justify-evenly">
-        <div class="row">
-          <div class="col-12">
-            <div class="row q-pt-xl text-center">
-              <div class="col-12">
-                <p>Tell us about your organization</p>
-              </div>
+      <q-page class="q-page row justify-center q-pa-sm">
+        <div class="col-xs-12 col-md-6">
+          <div class="row">
+            <div class="col-12 text-center">
+              <p>Tell us about your organization</p>
             </div>
-            <div class="row text-center">
-              <div class="col-12">
-                <h3 class="q-mt-none">Set up your team</h3>
-              </div>
+            <div class="col-12 text-center">
+              <h3 class="q-mt-none">Set up your team</h3>
             </div>
-            <div class="row q-pb-lg">
-              <div class="col-12">
-                <q-input required dense filled
-                  maxlength=32 counter
-                  label="Organization account name"
-                  v-model.trim="organization.name"
-                  :rules="[val => !!val || 'Field is required',
-                    val => val.length >= 4 || 'Please use between 4-32 characters',
-                    val => validateName(val) || 'Field may only contain alphanumeric characters and spaces']"
-                  :hint="'This will be the name of your account on ChangeLogs.Your URL will be: https://changelogs.info/'
-                    + organization.title"/>
-              </div>
-            </div>
-            <div class="row q-pb-lg">
-              <div class="col-12">
-                <q-input required dense filled label="Contact email"
-                  maxlength=64 counter
-                  :rules="[val => !!val || 'Field is required',
-                    val => val.length >= 8 || 'Please use between 8-64 characters',
-                    val => validateEmail(val) || 'Invalid email address']"
-                  v-model.trim="organization.email" />
-              </div>
-            </div>
-            <div class="row q-pb-lg">
-              <div class="col-12">
-                <q-file filled dense clearable counter bottom-slots nullable
-                  accept="image/*" max-file-size="500000"
-                  v-model="organizationLogo" label="Organization logo">
-                  <template v-slot:prepend>
-                    <q-icon name="attach_file" />
-                  </template>
-                  <template v-slot:hint>
-                    Only image under 512 KB
-                  </template>
-                </q-file>
-              </div>
-            </div>
-            <div class="row q-pb-lg">
-              <div class="col-12">
-                <q-input dense filled label="Slogan"
-                  maxlength=128 counter
-                  v-model.trim="organization.slogan" />
-              </div>
-            </div>
-            <div class="row q-pb-lg">
-              <div class="col-12">
-                <q-input dense filled label="Location"
-                  maxlength=64 counter
-                  v-model.trim="organization.location" />
-              </div>
-            </div>
-            <div class="row q-pb-lg">
-              <div class="col-12">
-                <q-input dense filled label="Website"
-                  maxlength=64 counter
-                  :rules="[val => validateURL(val, false) || 'Invalid URL']"
-                  v-model.trim="organization.website" />
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-12">
-                <q-btn dense color="primary" class="full-width"
-                  :disable="saveStatus"
-                  size="xl" label="Save" @click="saveOrganization"/>
+            <div class="col-12">
+              <div class="row q-col-gutter-md">
+                <div class="col-xs-12 col-md-6">
+                  <q-input required dense filled
+                    maxlength=32 counter
+                    label="Organization account name"
+                    v-model.trim="organization.name"
+                    :rules="[val => !!val || 'Field is required',
+                      val => val.length >= 4 || 'Please use between 4-32 characters',
+                      val => validateName(val) || 'Field may only contain alphanumeric characters and spaces']"
+                    :hint="'This will be the name of your account on ChangeLogs.Your URL will be: https://changelogs.info/'
+                      + organization.title"/>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                  <q-input required dense filled label="Contact email"
+                    maxlength=64 counter
+                    :rules="[val => !!val || 'Field is required',
+                      val => val.length >= 8 || 'Please use between 8-64 characters',
+                      val => validateEmail(val) || 'Invalid email address']"
+                    v-model.trim="organization.email" />
+                </div>
+                <div class="col-xs-12 col-md-6">
+                  <q-file filled dense clearable counter bottom-slots nullable
+                    accept="image/*" max-file-size="500000"
+                    v-model="organizationLogo" label="Organization logo">
+                    <template v-slot:prepend>
+                      <q-icon name="attach_file" />
+                    </template>
+                    <template v-slot:hint>
+                      Only image under 512 KB
+                    </template>
+                  </q-file>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                  <q-input dense filled label="Slogan"
+                    maxlength=128 counter
+                    v-model.trim="organization.slogan" />
+                </div>
+                <div class="col-xs-12 col-md-6">
+                  <q-input dense filled label="Location"
+                    maxlength=64 counter
+                    v-model.trim="organization.location" />
+                </div>
+                <div class="col-xs-12 col-md-6">
+                  <q-input dense filled label="Website"
+                    maxlength=64 counter
+                    :rules="[val => validateURL(val, false) || 'Invalid URL']"
+                    v-model.trim="organization.website" />
+                </div>
+                <div class="col-12">
+                  <q-btn dense color="primary" class="full-width"
+                    :disable="saveStatus"
+                    size="xl" label="Save" @click="saveOrganization"/>
+                </div>
               </div>
             </div>
           </div>
