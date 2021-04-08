5d8776bcf5f870926508028b2c802e52281eab93
GLinBoy <hojjatabedie@gmail.com>
2021-04-08 22:37:47 +0430

    👌 IMPROVE: Init index page presentation

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,59 +1,117 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
-  </q-page>
+  <q-layout>
+    <q-page-container>
+      <q-page class="q-page row justify-center q-pa-sm">
+        <div class="col-xs-12 col-md-6">
+          <div class="row">
+            <div class="col-12">
+              <span>latest changelogs:</span>
+            </div>
+            <div class="col-12">
+              <q-card class="my-card" flat
+                 v-for="change in changelogs" :key="change.key">
+                <q-item>
+                  <q-item-section avatar>
+                    <q-avatar>
+                      <img :src="change.project.logo">
+                    </q-avatar>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>{{ change.project.owner + '/' + change.project.title }}
+                      <q-chip square dense size="md">
+                        <q-avatar icon="lightbulb" color="blue" text-color="white" />
+                        {{ change.versionNo }}
+                      </q-chip>
+                      <q-chip square dense size="md">
+                        <q-avatar icon="settings" color="red" text-color="white" />
+                        {{ change.platform }}
+                      </q-chip>
+                    </q-item-label>
+                    <q-item-label caption>
+                      By <b>{{change.publisher}} @ {{ change.releaseDate }}</b>
+                    </q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-card-section horizontal>
+                  <q-card-section class="col-xs-2 col-md-1">
+
+                  </q-card-section>
+                  <q-card-section class="col-xs-10 col-ms-11">
+                    <q-card flat bordered>
+                      <q-card-section>
+                        <dl>
+                          <div v-for="content in change.contents" :key="content.id">
+                            <dt class="col-sm-3">{{ content.contentType }}</dt>
+                            <dd class="col-sm-9">{{ content.content }}</dd>
+                          </div>
+                        </dl>
+                      </q-card-section>
+                    </q-card>
+                  </q-card-section>
+                </q-card-section>
+              </q-card>
+            </div>
+          </div>
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models'
-import ExampleComponent from 'components/CompositionComponent.vue'
-import { defineComponent, ref } from '@vue/composition-api'
+import { ChangeLog, Pagination, Sort, SortDirection, CommonError } from 'components/models'
+import { defineComponent, ref, reactive } from '@vue/composition-api'
+import { AxiosError } from 'axios'
 
 export default defineComponent({
   name: 'PageIndex',
-  components: { ExampleComponent },
+  components: {},
   setup (_, context) {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ])
-    const meta = ref<Meta>({
-      totalCount: 1200
-    })
     const axios = context.root.$axios
-    axios.get('http://localhost:8080/api/v1/changelog')
+
+    const changelogs = ref<ChangeLog[]>([])
+
+    const pagination = reactive<Pagination>({
+      page: 0,
+      size: 20,
+      sort: [{
+        field: 'createdOn',
+        direction: SortDirection.DESC
+      }]
+    })
+
+    const urlTemplate = `changelog/latest?page=${pagination.page || 0}&size=${pagination.size || 20}&sort=${Array.prototype.map
+        .call(pagination.sort, function (s: Sort) { return `${s.field},${s.direction}` })
+        .join('&sort=')}`
+
+    axios.get<ChangeLog[]>(urlTemplate)
       .then(response => {
-        // handle success
-        console.log(response.data)
+        changelogs.value = response.data
       })
-      .catch(error => {
-        // handle error
-        console.log(error)
+      .catch((error: AxiosError) => {
+        console.error(error)
+        if (error.response && error.response.data) {
+          const errorData = <CommonError> error.response.data
+          context.root.$q.notify({
+            progress: true,
+            message: errorData.title,
+            caption: errorData.detail,
+            position: 'bottom-right',
+            color: 'negative',
+            icon: 'report_problem'
+          })
+        } else {
+          context.root.$q.notify({
+            progress: true,
+            message: 'Network Error',
+            caption: 'Can\'t access the APIs, please check your network, ant try again',
+            position: 'bottom-right',
+            color: 'negative',
+            icon: 'report_problem'
+          })
+        }
       })
-    return { todos, meta }
+    return { changelogs }
   }
 })
 </script>
