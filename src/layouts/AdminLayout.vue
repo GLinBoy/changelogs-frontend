3d107d36abd2c8ffaa3055525806849072956a24
GLinBoy <hojjatabedie@gmail.com>
2021-12-27 20:13:52 +0330

    👌 IMPROVE: Upgraded admin layout to Quasar 2

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -55,63 +55,62 @@
 </template>
 
 <script lang="ts">
-import EssentialLink from 'components/EssentialLink.vue'
+import EssentialLink from 'components/EssentialLink.vue';
 
 const linksData = [
   {
     title: 'Docs',
     caption: 'quasar.dev',
     icon: 'school',
-    link: 'https://quasar.dev'
+    link: 'https://quasar.dev',
   },
   {
     title: 'Github',
     caption: 'github.com/quasarframework',
     icon: 'code',
-    link: 'https://github.com/quasarframework'
+    link: 'https://github.com/quasarframework',
   },
   {
     title: 'Discord Chat Channel',
     caption: 'chat.quasar.dev',
     icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    link: 'https://chat.quasar.dev',
   },
   {
     title: 'Forum',
     caption: 'forum.quasar.dev',
     icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    link: 'https://forum.quasar.dev',
   },
   {
     title: 'Twitter',
     caption: '@quasarframework',
     icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    link: 'https://twitter.quasar.dev',
   },
   {
     title: 'Facebook',
     caption: '@QuasarFramework',
     icon: 'public',
-    link: 'https://facebook.quasar.dev'
+    link: 'https://facebook.quasar.dev',
   },
   {
     title: 'Quasar Awesome',
     caption: 'Community Quasar projects',
     icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
-
-import { defineComponent, ref } from '@vue/composition-api'
+    link: 'https://awesome.quasar.dev',
+  },
+];
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'MainLayout',
   components: { EssentialLink },
-  setup () {
-    const leftDrawerOpen = ref(false)
-    const essentialLinks = ref(linksData)
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const essentialLinks = ref(linksData);
 
-    return { leftDrawerOpen, essentialLinks }
-  }
-})
+    return { leftDrawerOpen, essentialLinks };
+  },
+});
 </script>
